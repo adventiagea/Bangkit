7d1f097a72b73013d0df8e712378f078f298697f
adventiagea <adventiahaganigea@gmail.com>
2021-06-03 03:31:41 +0700

    Mengubah warna Hint Text pada Username dan Password, Mengkonfigurasikan Tanggal

--- android v4.2/app/src/main/java/com/dicoding/picodiploma/libas/PredictionActivity.kt
+++ android v4.2/app/src/main/java/com/dicoding/picodiploma/libas/PredictionActivity.kt
@@ -1,11 +1,40 @@
 package com.dicoding.picodiploma.libas
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.picodiploma.libas.databinding.ActivityPredictionBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class PredictionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPredictionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_prediction)
+        binding = ActivityPredictionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dateText = binding.waktuPrediksi
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val date = LocalDate.now()
+            val dateFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            dateText.text = dateFormat.format(date)
+        } else {
+            fun Date.toString(format : String, locale: Locale = Locale.getDefault()): String {
+                val dateFormat = SimpleDateFormat(format, locale)
+                return dateFormat.format(this)
+            }
+            fun getCurrentDateTime(): Date {
+                return Calendar.getInstance().time
+            }
+
+            val date = getCurrentDateTime()
+            val dateInString = date.toString("dd-MM-yyyy")
+
+            dateText.text = dateInString
+        }
+
     }
 }
